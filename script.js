6a32f1de076cca1177ee85c6e73ce0ffdb7344b4
argodion <59009426+argodion@users.noreply.github.com>
2019-12-18 14:11:15 +0100

    Update #1
    Still work in progress, but getting somewhere

--- script.js
+++ script.js
@@ -1,54 +1,74 @@
 var todolist = document.getElementById("list");
 var iteration = todolist.getElementsByTagName("li").length;
-var editButtons = [];
+//var editButtons = [];
+var pValues = []; //Stores the values of each list item
 
-function newListItem(listID) {
-  listID.innerHTML +=
-    '<li id="listitem' +
-    iteration +
-    '" nmbr="' +
-    iteration +
-    '" class="listItem"> <input type="text" autofocus required /><button id="resetBtn">x</button><button id="submitBtn">►</button> </li>';
+function newListItem(func, liNmbr, value) {
+  if (func === "new") {
+    todolist.innerHTML +=
+      '<li id="listitem' +
+      iteration +
+      '" nmbr="' +
+      iteration +
+      '" class="listItem"></li>';
+  } else if (func === "edit") {
+    let li = document.getElementById("listitem" + liNmbr);
+  } else {
+    alert("something went wrong :(");
+  }
+  console.log("newlistitem 1 " + iteration);
+  editListItem(iteration, false);
+
   iteration++;
+  console.log("newlistitem 2 " + iteration);
+}
 
-  document.getElementById("submitBtn").onclick = setListitem;
+function editListItem(liNmbr, fromBtn) {
+  console.log("editlistitem 1 " + iteration);
+  let listItem = document.getElementById("listitem" + liNmbr);
+  console.log(listItem);
+  let lastListItem = document.getElementById("listitem" + iteration);
+  let value = "";
+
+  if (fromBtn) {
+    value = pValues[liNmbr];
+    Array.from(lastListItem.children).forEach(element =>
+      element.setAttribute("disabled", "")
+    );
+  }
+
+  listItem.innerHTML =
+    '<input type="text" autofocus required value="' +
+    value +
+    '" /><button id="resetBtn">x</button><button id="submitBtn">►</button>';
+
+  document.getElementById("submitBtn").onclick = function() {
+    console.log(this.parentElement.firstChild.value);
+    if (value === "") {
+      setListitem("new", liNmbr, this.parentElement.firstChild.value);
+    } else {
+      setListitem("edit", liNmbr, this.parentElement.firstChild.value);
+    }
+  };
   document.getElementById("resetBtn").onclick = function() {
     this.previousElementSibling.value = "";
   };
 }
 
-function editListItem(btn) {
-  console.log(btn);
-}
-
-function setListitem() {
-  let i = iteration - 1;
-  let li = document.getElementById("listitem" + i);
-  let liValue = li.getElementsByTagName("input")[0].value;
+function setListitem(func, liNmbr, value) {
+  let li = document.getElementById("listitem" + liNmbr);
+  pValues[liNmbr] = value; //li.getElementsByTagName("input")[0].value;
 
   li.innerHTML =
     '<p class="liText">' +
-    liValue +
-    '</p> <button class="editBtn" nmbr="' +
-    i +
-    '" >edit</button>';
+    pValues[liNmbr] +
+    '</p> <button class="editBtn" onclick="editListItem(' +
+    liNmbr +
+    ', true)" >edit</button>';
 
-  editButtons = document.getElementsByClassName("editBtn");
+  //editButtons = document.getElementsByClassName("editBtn");
 
-  editButtons.forEach(function() {
-    console.log(this);
-  });
-
-  // for (var j = 0; j < editButtons.length; j++) {
-  //   console.log("forloop " + j);
-  //   console.log(editButtons[j]);
-  //   editButtons[j].onclick = function() {
-  //     console.log("I'm in");
-  //     editListItem(this);
-  //   };
-  // }
-
-  newListItem(todolist);
+  newListItem(func, liNmbr, value);
 }
 
-newListItem(todolist);
+newListItem("new");
